1607d318c88bf12bffe1f2f20e971378990e5cb0
sergejdarin <Darserg1212@mail.ru>
2023-07-25 16:05:54 +0400

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Сергей Дарьин on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
