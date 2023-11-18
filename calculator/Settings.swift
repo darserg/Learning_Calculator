e3640f2cb2571c2b6c4040f052b33f98086b338b
darserg <darserg1212@mail.ru>
2023-11-18 09:36:13 +0400

    Added settings screen

---
+++ calculator/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  calculator
+//
+//  Created by Сергей Дарьин on 05.11.2023.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings()
+}
